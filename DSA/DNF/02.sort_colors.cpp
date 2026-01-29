2c46c689b8aef68aa699dcca3182cee0c1511c64
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-01-29 09:34:18 +0530

    modified:   DSA/DNF/02.sort_colors.cpp 	modified:   DSA/Patterns/Sliding Window/04_min_swaps.cpp 	modified:   DSA/Patterns/Two_Pointer.cpp/08.merge_sorted_array.cpp

--- DSA/DNF/02.sort_colors.cpp
+++ DSA/DNF/02.sort_colors.cpp
@@ -1,1 +1,21 @@
 //  75 leetcode
+// https://leetcode.com/problems/sort-colors/submissions/1900406714/
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>&arr) {
+//         int n=arr.size();
+//         int low=0;
+//         int high=n-1;
+//         int mid=0;
+//         while(mid<=high){
+//             if(arr[mid]==0){
+//                 swap(arr[mid++],arr[low++]);
+//             }else if(arr[mid]==1){
+//                 mid++;
+//             }else{
+//                 swap(arr[mid],arr[high--]);
+//             }
+//         }
+//     }
+// };
--- DSA/Patterns/Sliding Window/04_min_swaps.cpp
+++ DSA/Patterns/Sliding Window/04_min_swaps.cpp
@@ -25,3 +25,53 @@
 //         return count-maxones;
 //     }
 // };                 
+
+// class Solution {
+// public:
+//     int minSwaps(vector<int>& nums) {
+//         int n=nums.size();
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==1){
+//                 count++;
+//             }
+//         }
+//          if(count <= 1) return 0;
+//         int ones=0;
+//         for(int i=0;i<count;i++){
+//             if(nums[i]==1){
+//                 ones++;
+//             }
+//         }
+//         int maxones=ones;
+//         for(int i=count;i<n;i++){
+//             if (nums[i]==1) ones++;
+//             if(nums[i-count]==1) ones--;
+//             maxones=max(maxones,ones);
+//         }
+//         return count-maxones;
+
+//         int n = nums.size();
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==1){
+//                 count++;
+//             }
+//         }
+//         if(count <= 1) return 0;
+//         vector<int> arr = nums;
+//         arr.insert(arr.end(), nums.begin(), nums.end());
+    
+//         int ones = 0;
+//         for(int i = 0; i < count; i++) {
+//             if(arr[i] == 1) ones++;
+//         }
+//         int maxOnes = ones;
+//         for(int i = count; i < count + n; i++) {
+//             if(arr[i] == 1) ones++;
+//             if(arr[i - count] == 1) ones--;
+//             maxOnes = max(maxOnes, ones);
+//         }
+//         return count - maxOnes;
+//     }
+// };
