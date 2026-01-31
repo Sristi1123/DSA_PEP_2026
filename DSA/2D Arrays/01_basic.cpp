3af834bb156d3d02d88ace1512c5a78c489e461b
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-01-31 13:53:33 +0530

    new file:   DSA/2D Arrays/01_basic.cpp 	new file:   DSA/2D Arrays/01_search.cpp

---
+++ DSA/2D Arrays/01_basic.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int arr[3][4]={{2,3,4,5},{6,4,2,1},{7,0,4,8}};
+    // int arr[3][4]={1,2,3,4,5,6,7,8,9,1,2,3};
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    int m;
+    cout<<"Enter m: ";
+    cin>>m;
+    int arr[n][m];
+
+    // taking input row-wise
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cin>>arr[i][j];
+    //     }
+    // }
+
+    // taking input col-wise
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[j][i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ DSA/2D Arrays/01_search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+bool search(int arr[][4],int key){        // row major concept  
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(arr[i][j]==key){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int main(){
+    int arr[][4]={
+        {1,7,14,6},
+        {11,15,5,10},
+        {2,4,16,3},
+        {9,13,8,12}
+    };
+    int key=17;
+    cout<<search(arr,key);
+    return 0;
+}
