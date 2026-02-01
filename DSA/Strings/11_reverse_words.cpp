4fcf2f7de817375a67dfcf9038d51fa691b0bdd4
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-02-01 21:58:52 +0530

    modified:   DSA/Strings/11_reverse_words.cpp

--- DSA/Strings/11_reverse_words.cpp
+++ DSA/Strings/11_reverse_words.cpp
@@ -1,2 +1,26 @@
 // https://leetcode.com/problems/reverse-words-in-a-string/
 
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         int n = s.length();
+//         int start = 0;
+//         reverse(s.begin(), s.end());
+//         for (int i = 0; i <= n; i++) {
+//             if (i == n || s[i] == ' ') {
+//                 reverse(s.begin() + start, s.begin() + i);
+//                 start = i + 1;
+//             }
+//         }
+//         string res = "";
+//         int i = 0;
+//         while (i < n) {
+//             while (i < n && s[i] == ' ') i++;
+//             while (i < n && s[i] != ' ') res += s[i++];
+//             while (i < n && s[i] == ' ') i++;
+//             if (i < n) res += ' ';
+//         }
+
+//         return res;
+//     }
+// };
