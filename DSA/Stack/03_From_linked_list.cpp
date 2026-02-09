92da7929660f574fa09dbb1adfafaf547cff49c7
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-02-09 12:53:11 +0530

    new file:   DSA/Stack/03_From_linked_list.cpp

---
+++ DSA/Stack/03_From_linked_list.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+class MyStack{
+    public:
+    Node* top;
+    MyStack(){
+        top=NULL;
+    }
+    void push(int val){
+        Node* node=new Node(val);
+        node->next=top;
+        top=node;
+    }
+    void pop(){
+        if(top==NULL){
+            cout<<"stack underflow";
+            return;
+        }
+        Node* temp=top;
+        top=top->next;
+        delete top;
+    }
+    int peek(){
+        if(top==NULL){
+            cout<<"no element";
+            return;
+        }
+        return top->data;
+        // Node* temp=top;
+        // while(temp!=NULL){
+        //      temp=temp->next;
+        // }
+        // return temp->data;
+    }
+    bool isEMpty(){
+        return top==NULL;
+    }
+};
+int main(){
+    
+    return 0;
+}
