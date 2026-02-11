b4ec1631ba7cb397585c4cc717d0c31f7458e779
Sristi1123 <144992720+Sristi1123@users.noreply.github.com>
2026-02-11 18:24:35 +0530

    new file:   Practice/08_squares_of_sorted_array.cpp

---
+++ Practice/08_squares_of_sorted_array.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> ans(n);
+//         // for(int i=0;i<n;i++){
+//         //     ans[i]=nums[i]*nums[i];
+//         // }
+//         // sort(ans.begin(),ans.end());
+//         // return ans;
+//         int left=0;
+//         int right=n-1;
+//         int pos=n-1;
+//         while(left<=right){
+//             if(abs(nums[left])>abs(nums[right])){
+//                 ans[pos]=nums[left]*nums[left];
+//                 left++;
+//             }else {
+//                 ans[pos]=nums[right]*nums[right];
+//                 right--;
+//             }
+//             pos--;
+//         }
+//         return ans;
+//     }
+// };
