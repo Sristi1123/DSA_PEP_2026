b3c09b768e0416bc261744887c0ccd452f1988e1
Sristi1123 <sristi1501@gmail.com>
2026-02-15 19:24:52 +0530

    new file:   DSA/Tree/09_invertTree.cpp

---
+++ DSA/Tree/09_invertTree.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/invert-binary-tree/?envType=problem-list-v2&envId=tree
+
+// class Solution {
+// public:
+//     TreeNode* invertTree(TreeNode* root) {
+//         if(root==NULL){
+//             return root;
+//         }
+//         TreeNode* x =root->left;
+//         root->left=root->right;
+//         root->right=x;
+//         invertTree(root->left);
+//         invertTree(root->right);
+//         return root;
+//     }
+// };
