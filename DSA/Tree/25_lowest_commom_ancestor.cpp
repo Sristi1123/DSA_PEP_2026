996010daed8e4ff1d0cb791ef150ae8e1a154ae0
Sristi1123 <sristi1501@gmail.com>
2026-02-18 21:55:19 +0530

    save 	# modified:   DSA/Tree/25_lowest_commom_ancestor.cpp

--- DSA/Tree/25_lowest_commom_ancestor.cpp
+++ DSA/Tree/25_lowest_commom_ancestor.cpp
@@ -5,13 +5,18 @@
 // public:
 //     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 //         if(root==NULL) return NULL;
+
+        // agar root hi p ya q ho toh yahi ek ancestor ho sakta hai
 //         if(root==p|| root==q) return root;
+            
 //         TreeNode* left=lowestCommonAncestor(root->left,p,q);
 //         TreeNode* right=lowestCommonAncestor(root->right,p,q);
 
+ // agar p ek side mein aur q doosri side mein mila toh current root hi LCA hoga
 //         if(left && right){
 //             return root;
 //         }
+// agar dono left subtree mein mile
 //         else if(left!=NULL) return left;
 //         else return right;
 //     }
