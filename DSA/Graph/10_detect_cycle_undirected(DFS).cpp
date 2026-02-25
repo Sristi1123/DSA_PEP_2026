a01d72770657d357c22508c23d9046fbbccae991
Sristi1123 <sristi1501@gmail.com>
2026-02-25 07:44:26 +0530

    save

---
+++ DSA/Graph/10_detect_cycle_undirected(DFS).cpp
@@ -0,0 +1,35 @@
+// https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+// class Solution {
+//   public:
+//     bool dfs(int start,unordered_map<int,int> &parent,vector<bool> &visited,vector<vector<int>> &adj){
+//         visited[start]=true;
+//         for(int i :adj[start]){
+//             if(!visited[i]){
+//                 parent[i]=start;
+//                 if(dfs(i,parent,visited,adj)) return true;
+//             }
+//             else if(parent[start]!=i) return true;
+//         }
+//         return false;
+//     }
+//     bool isCycle(int V, vector<vector<int>>& edges) {
+//         // Code here
+//         vector<vector<int>>adj(V);
+//         for(auto &it:edges){
+//             int u=it[0];
+//             int v=it[1];
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+//         vector<bool> visited(V,false);
+//         for(int i=0;i<V;i++){
+//             if(!visited[i]){
+//                 unordered_map<int,int> parent;
+//                 parent[i]=-1;
+//                 if(dfs(i,parent,visited,adj)) return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
