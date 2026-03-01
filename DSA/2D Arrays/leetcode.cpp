64b38e650633bddf173445255202a2a2b030e20a
Sristi1123 <sristi1501@gmail.com>
2026-03-01 17:36:13 +0530

    modified:   DSA/2D Arrays/leetcode.cpp

--- DSA/2D Arrays/leetcode.cpp
+++ DSA/2D Arrays/leetcode.cpp
@@ -1,2 +1,14 @@
 // https://www.geeksforgeeks.org/problems/two-dimensional-world/1
 
+// void twoDimensional(int **arr, int N) {
+
+//     // Loop to iterate through matrix
+//     // and print all the matrix elements row wise
+//     // int m=arr[0].size();
+//     for(int i=0;i<N;i++){
+//         for(int j=0;j<N;j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
