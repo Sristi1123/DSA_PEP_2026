06f73b55b79352e2cf58769ec9cbb3eaf8cd5c1e
Sristi1123 <sristi1501@gmail.com>
2026-03-10 19:00:14 +0530

    save

---
+++ Practice/43_longest_common_prefic.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         int n=strs.size();
+//         for(int i=0;i<strs[0].length();i++){
+//             char c=strs[0][i];
+//             for(int j=1;j<n;j++){
+//                 if(i>=strs[j].length()||strs[j][i]!=c){
+//                     return strs[0].substr(0,i);
+//                 }
+//             }
+//         }
+//         return strs[0];
+//     }
+// };
